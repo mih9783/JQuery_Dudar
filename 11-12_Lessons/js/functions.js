39aec7f9dffb966a9f329bbb6b53232af8f0e790
mih9783 <mih9783@yandex.ru>
2019-11-19 19:02:39 +0700

    17 урок закончил

--- 11-12_Lessons/js/functions.js
+++ 11-12_Lessons/js/functions.js
@@ -57,20 +57,71 @@ $(function () {
     //     question = prompt("Какой цвет вам больше нравится зелёный,красный или жёлтый");
     // }
 
-    // $("#test").attr("class", "test_" + question);
+    //  $("#test").attr("class", "test_" + question);
 
-    let div = prompt("Введите 1, 2 или 3"),
-    one = 1,
-    two = 2,
-    three = 3;
 
-    while (div != one && div != two && div != three) {
-        alert("Вы не ввели пароль");
-        div = prompt("Введите 1, 2 или 3");
+    ///////////////  РАБОТАЕТ //////////////////
+
+    // let Color = prompt("Какой цвет вам больше нравится? green,red или yellow");
+
+
+
+    // while (Color != "red" && Color != "green" && Color != "yellow") {
+    //     alert("Вы не выбрали цвет");
+    //     Color = prompt("Какой цвет вам больше нравится зелёный,красный или жёлтый");
+    // }
+
+
+
+    // $("#test").attr("class", "test_" + Color);
+    // alert($("#test_").get(0).className);
+
+    ///////////////////////
+
+    var question = prompt("Какой цвет вам больше нравится зелёный,красный или жёлтый"),
+        green = "зелёный",
+        red = "красный",
+        yellow = "жёлтый";
+
+    while (question != green && question != yellow && question != red) {
+        alert("Вы не выбрали цвет");
+        question = prompt("Какой цвет вам больше нравится зелёный,красный или жёлтый");
     }
 
-    $("#test-1").attr ("class", "div_" + div);
-    alert($("#test-1").get(0).className);
+    if (question === green)
+        $("#test").attr("class", "test_" + "green");
+    if (question === red)
+        $("#test").attr("class", "test_" + "red");
+    if (question === yellow)
+        $("#test").attr("class", "test_" + "yellow");
+
+
+
+
+
+    // let div = prompt("Введите 1, 2 или 3");
+
+    // while (div != 1 && div != 2 && div != 3) {
+    //     alert("Вы не ввели пароль");
+    //     div = prompt("Введите 1, 2 или 3");
+    // }
+
+    // $("#test-1").attr ("class", "div_" + div);
+    // alert($("#test-1").get(0).className);
+
+
+    // let div = prompt("Введите 1, 2 или 3"),
+    // one = 1,
+    // two = 2,
+    // three = 3;
+
+    // while (div != one && div != two && div != three) {
+    //     alert("Вы не ввели пароль");
+    //     div = prompt("Введите 1, 2 или 3");
+    // }
+
+    // $("#test-1").attr ("class", "div_" + div);
+    // alert($("#test-1").get(0).className);
 
     // get(0) обращаемся по индексу  к диву #test-1
 
