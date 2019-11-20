a814a80c36f60c2e9aeb60611397c03932fbed74
mih9783 <mih9783@yandex.ru>
2019-11-20 19:18:07 +0700

    22 урок закончил

--- 21-22_Lessons/js/functions.js
+++ 21-22_Lessons/js/functions.js
@@ -11,11 +11,50 @@
 
 // });
 
-
-
 $(function () {
 
-    alert('jQuery работает');
+    $(document).on("click", "#add_comment", function () {
+        let form = $("form[name='form']");
+        form.css("display", "block");
+        // заменяем ссылку на форму
+        $("#add_comment").replaceWith(form);
+    });
+
+    // берём содержимое textarea и вставляем его в комментарии
+
+    $(document).on("click", "input[name='send']", function () {
+        let comment = $("textarea[name='comment']").val();
+        alert("Подождите, идёт загрузка комментария ...");
+        // Добавляем комменты в конец
+        $("<p>" + comment + "</p>").appendTo("#comments");
+    });
+
+    function hideComments() {
+        $("#comments-1").hide(1000, function () {
+            $("#hide").hide();
+            $("#show").show();
+        });
+    }
+
+    function showComments() {
+        $("#hide").show();
+        $("#show").hide();
+        $("#comments-1").show(1000);
+    }
+
+    // или так тоже работает
+
+    // function showComments() {
+    //     $("#comments-1").show(1000, function () {
+    //         $("#hide").show();
+    //         $("#show").hide();
+    //     });
+    // }
+
+    $("#hide").bind("click", hideComments);
+    $("#show").bind("click", showComments);
+
+
 
 });
 
