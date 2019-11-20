c1d73077b20216dfb684ee832585f0fc108335d1
mih9783 <mih9783@yandex.ru>
2019-11-20 14:28:11 +0700

    21 урок начало

---
+++ 21-22_Lessons/js/functions.js
@@ -0,0 +1,23 @@
+//  проверяю работу jquery
+
+// $(document).ready(function () {
+//     alert('jQuery работает');
+// });
+
+// ИЛИ СОКРАЩЁННО
+
+// $(function () {
+//     alert('jQuery работает');
+
+// });
+
+
+
+$(function () {
+
+    alert('jQuery работает');
+
+});
+
+
+
