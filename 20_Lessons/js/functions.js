eb6c503ce4ab560953004f4e0c8dbb5af769e585
mih9783 <mih9783@yandex.ru>
2019-11-20 08:16:07 +0700

    20 урок начало

---
+++ 20_Lessons/js/functions.js
@@ -0,0 +1,27 @@
+//  проверяю работу jquery
+
+// $(document).ready(function () {
+//     alert('jQuery работает');
+// });
+
+// ИЛИ СОКРАЩЁННО
+
+// $(function () {
+//     alert('jQuery работает');
+
+// });
+
+$(function () {
+
+    // Обработчик событии
+
+    // $(".btn btn_width-block").click(function () {
+    //     let width = $(".btn btn_width-red, .btn btn_width-green, btn btn_width-blue");
+    //     $(width).css("width", "4rem");
+    // });
+
+    
+});
+
+
+
