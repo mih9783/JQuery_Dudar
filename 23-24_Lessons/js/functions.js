49b4a5b096ce422bbd33938b60be8f44269012c4
mih9783 <mih9783@yandex.ru>
2019-11-25 06:49:01 +0700

    24 урок закончил

--- 23-24_Lessons/js/functions.js
+++ 23-24_Lessons/js/functions.js
@@ -13,24 +13,8 @@
 
 $(function () {
 
-    $(document).on("click", "#add_comment", function () {
-        let form = $("form[name='form']");
-        form.css("display", "block");
-        // заменяем ссылку на форму
-        $("#add_comment").replaceWith(form);
-    });
-
-    // берём содержимое textarea и вставляем его в комментарии
-
-    $(document).on("click", "input[name='send']", function () {
-        let comment = $("textarea[name='comment']").val();
-        alert("Подождите, идёт загрузка комментария ...");
-        // Добавляем комменты в конец
-        $("<p>" + comment + "</p>").appendTo("#comments");
-    });
-
     function hideComments() {
-        $("#comments-1").hide(1000, function () {
+        $("#comments").hide(1000, function () {
             $("#hide").hide();
             $("#show").show();
         });
@@ -39,13 +23,13 @@ $(function () {
     function showComments() {
         $("#hide").show();
         $("#show").hide();
-        $("#comments-1").show(1000);
+        $("#comments").show(1000);
     }
 
     // или так тоже работает
 
     // function showComments() {
-    //     $("#comments-1").show(1000, function () {
+    //     $("#comments").show(1000, function () {
     //         $("#hide").show();
     //         $("#show").hide();
     //     });
@@ -53,6 +37,58 @@ $(function () {
 
     $("#hide").bind("click", hideComments);
     $("#show").bind("click", showComments);
+
+    // красивая анимация  
+
+    function hideComment() {
+        $("#comment").slideUp(1000, function () {
+            $("#hide-1").hide();
+            $("#show-1").show();
+        });
+    }
+
+    function showComment() {
+        $("#hide-1").slideDown();
+        $("#show-1").hide();
+        $("#comment").show(1000);
+
+    }
+
+    $("#hide-1").bind("click", hideComment);
+    $("#show-1").bind("click", showComment);
+
+    function hideComment1() {
+        $("#comment-2").fadeOut(1000, function () {
+            $("#hide-2").hide();
+            $("#show-2").show();
+        });
+    }
+
+    function showComment1() {
+        $("#hide-2").fadeIn();
+        $("#show-2").hide();
+        $("#comment-2").show(1000);
+
+    }
+
+    $("#hide-2").bind("click", hideComment1);
+    $("#show-2").bind("click", showComment1);
+
+    // 24 урок. Анимация
+
+    $("#img").bind("click", function (event) {
+        $(event.target).animate({
+            width: $(event.target).width() * 4,
+            //height: $(event.target).width() * 5
+
+        }, 5000);
+    });
+
+    $(document).on("click", "#img-1", function () {
+        $(event.target).animate({
+            width: $(event.target).width() * 4,
+        }, 5000);
+    });
 
 
 
